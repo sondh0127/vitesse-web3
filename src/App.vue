d25c74a74ec51b936fccd962131f9f1335f6e6a3
Son Hong Do <sondh0127@gmail.com>
2022-04-03 16:44:43 +0700

    add ui for wave

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,8 @@
 <script setup lang="ts">
 // https://github.com/vueuse/head
 // you can use this to manipulate the document head in any components,
+import { useCryptoStore } from './stores/crypto'
+
 // they will be rendered correctly in the html results with vite-ssg
 useHead({
   title: 'Vitesse',
@@ -8,8 +10,12 @@ useHead({
     { name: 'description', content: 'Opinionated Vite Starter Template' },
   ],
 })
+
+const crypto = useCryptoStore()
+
 </script>
 
 <template>
+  <Loader v-if="crypto.loading" />
   <router-view />
 </template>
--- src/pages/index.vue
+++ src/pages/index.vue
@@ -1,5 +1,8 @@
 <script setup lang="ts">
+import { storeToRefs } from 'pinia'
 import { useUserStore } from '~/stores/user'
+
+import { useCryptoStore } from '~/stores/crypto'
 
 const user = useUserStore()
 const name = $ref(user.savedName)
@@ -9,11 +12,46 @@ const go = () => {
   if (name)
     router.push(`/hi/${encodeURIComponent(name)}`)
 }
+const messageInput = ref(null as any)
+const cryptoStore = useCryptoStore()
+const { wave, connectWallet } = useCryptoStore()
+const { account, guestPosts, guestPostsCount } = storeToRefs(cryptoStore)
 
 const { t } = useI18n()
 </script>
 
 <template>
+  <div class="flex flex-col items-center">
+    <h1 class="text-2xl m-4">
+      sondh0127's Crypto Guest Book
+    </h1>
+    <button v-if="!account" class="bg-green-300 rounded p-4" @click="connectWallet">
+      Connect Wallet
+    </button>
+    <div v-if="account" class="mt-5">
+      <input
+        v-model="messageInput"
+        name="guestBookInfo"
+        class="py-4 px-4  shadow border rounded"
+        maxlength="20"
+      >
+      <button class="bg-yellow-300 rounded p-4 mt-10" @click="wave(messageInput)">
+        Send
+      </button>
+    </div>
+
+    <div v-if="account" class="border shadow  w-4/12 p-4 mt-10">
+      <h3 class="text-2xl">
+        Number Of Entries: {{ guestPostsCount }}
+      </h3>
+      <div v-for="(guestPost, idx) in guestPosts" :key="idx" class="flex flex-col  m-auto " :class="{'mt-4': idx > 1}">
+        <div v-if="guestPost.message" class="flex justify-between w-full">
+          <span class="font-semibold">{{ guestPost.timestamp }}</span>
+          <span>{{ guestPost.message }}</span>
+        </div>
+      </div>
+    </div>
+  </div>
   <div>
     <div text-4xl>
       <div i-carbon-campsite inline-block />
